c26a1b9804fe116a77a7f9dcb5ecc29ae69a6af4
Oriol Soler <oriol.soler.cabot@hotmail.com>
2017-10-16 09:16:15 +0200

    main

--- main.cpp
+++ main.cpp
@@ -4,10 +4,20 @@
 #include "./include/Rectangle.h"
 #include "./include/Dibuix.h"
 
+using namespace std;
 int main() {
     Rectangle rect (1,1,8,4,2,3);
     rect.toString();
 
+
+    FiguraGeometrica *s;
+    s = new Rectangle(1,1,8,4,2,3);
+
+    Dibuix *e;
+
+    e = new Dibuix(9,9,s);
+    e->toString();
+    cout << "HELLLLLLLO";
 
     return 0;
 }
